fc77f0633cc9c1e716e395a3fb6a685a6763dc06
Aranccar <49247676+Aranccar@users.noreply.github.com>
2026-05-04 16:09:13 +0600

    Add: insert-delete-getrandom-o1 - submission-1

---
+++ Data Structures & Algorithms/insert-delete-getrandom-o1/submission-1.java
@@ -0,0 +1,33 @@
+public class RandomizedSet {
+    private Map<Integer, Integer> numMap;
+    private List<Integer> nums;
+    private Random rand;
+
+    public RandomizedSet() {
+        numMap = new HashMap<>();
+        nums = new ArrayList<>();
+        rand = new Random();
+    }
+
+    public boolean insert(int val) {
+        if (numMap.containsKey(val)) return false;
+        numMap.put(val, nums.size());
+        nums.add(val);
+        return true;
+    }
+
+    public boolean remove(int val) {
+        if (!numMap.containsKey(val)) return false;
+        int idx = numMap.get(val);
+        int last = nums.get(nums.size() - 1);
+        nums.set(idx, last);
+        numMap.put(last, idx);
+        nums.remove(nums.size() - 1);
+        numMap.remove(val);
+        return true;
+    }
+
+    public int getRandom() {
+        return nums.get(rand.nextInt(nums.size()));
+    }
+}

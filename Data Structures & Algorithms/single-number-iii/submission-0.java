52c3e38c8273551a8505a6b75235283b4154ac79
Aranccar <49247676+Aranccar@users.noreply.github.com>
2026-05-07 20:23:49 +0600

    Add: single-number-iii - submission-0

---
+++ Data Structures & Algorithms/single-number-iii/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int xor = 0;
+        for (int num : nums) {
+            xor ^= num;
+        }
+
+        int diff_bit = xor & (-xor);
+
+        int a = 0, b = 0;
+        for (int num : nums) {
+            if ((num & diff_bit) != 0) {
+                a ^= num;
+            } else {
+                b ^= num;
+            }
+        }
+        return new int[]{a, b};
+    }
+}
